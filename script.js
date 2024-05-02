1598c7e5f7679ed2cfdacdbb2492170e773c0295
natsmartin <natsmartin95@gmail.com>
2024-05-03 07:41:45 +0800

    Add styles, media queries, js script

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let x = document.getElementById("navigation-mobile-screen");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
